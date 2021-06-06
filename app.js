0a83064b8093e4911df150c229f5388489cdc9c2
walaamohammad <layan_abadi86@yahoo.com>
2021-06-06 16:24:16 +0300

    add

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 let userName =prompt('what is your name?');
 alert ('hello' + userName );
+console.log(userName);
+
 let place = prompt('where are you from'?);
+alert ('welcome');
 console.log(`your place is ${place}, beatiful`);
+
 const film = prompt('what is your favoirate film?');
 alert('your'+ film +'is beautiful');
+console.log(film);
+
 let nickName =prompt('what is your nickname in your family?');
 alert ('your'+ nickName + 'is funny');
+console.log(nickName);
