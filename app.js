753f1b9765b093d2d43aaa6b1f24a867af4a85cf
walaamohammad <layan_abadi86@yahoo.com>
2021-06-06 15:45:33 +0300

    change file name

---
+++ app.js
@@ -0,0 +1,8 @@
+let userName =prompt('what is your name?');
+aler ('hello' + userName );
+let place = prompt('where are you from'?);
+console.log(`your place is ${place},is beatiful`);
+const film = prompt('what is your favoirate film?');
+alert('your'+ film +'is beautiful');
+let nickName =prompt('what is your nickname in your family?');
+alert ('your'+ nickName + 'is funny');
